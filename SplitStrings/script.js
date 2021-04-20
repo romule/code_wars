2ec8dc4b8dfb0014b04b9676204d533595b4f14e
Elijah Romashchenko <romashbis@gmail.com>
2021-04-20 16:26:30 +0300

    String Split

--- SplitStrings/script.js
+++ SplitStrings/script.js
@@ -1,11 +1,12 @@
 // f() for start
-
 function solution(str) {
-  return oddOrnot(str);
+  if (str.length % 2 === 0) return odd(str);
+  else return notOdd(str);
 }
 
 // f() if string length is odd
-function odd(x, newArr) {
+function odd(x) {
+  let newArr = [];
   for (let i = 0; i < x.length; i++) {
     if (i % 2 === 0) {
       newArr[i] = x[i] + x[i + 1];
@@ -20,28 +21,14 @@ function odd(x, newArr) {
 }
 
 // f() if string length is NOT! odd
-function notOdd(x, newArr) {
-  console.log(x.length);
-  for (let i = 0; i < x.length; i++) {
-    console.log(`x${i} = ` + x[i]);
-
-    if (i % 2 === 0) {
-      newArr[i] = x[i] + x[i + 1];
-    }
-  }
-  console.log(x.length);
-
-  let filtr = newArr.filter(function (e) {
-    return e != null;
-  });
-
-  return filtr;
-}
-
-function oddOrnot(x) {
+function notOdd(x) {
   let newArr = [];
-  if (x.length % 2 === 0) return odd(x, newArr);
-  else return notOdd(x, newArr);
+  for (let i = 0; i < x.length + 1; i++) {
+    newArr[i] = x[i];
+    if (i === x.length) newArr[i] = "_";
+  }
+
+  return odd(newArr);
 }
 
 console.log(solution("abcdef"));
